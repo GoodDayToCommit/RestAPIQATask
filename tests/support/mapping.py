2cecf6eb1ca3e2c68b2aec8c06a27ced2c407443
azamat <azamat.imaev.work@gmail.com>
2023-05-16 12:32:30 +0300

    Test cases reorganized, new help functions were added

--- tests/support/mapping.py
+++ tests/support/mapping.py
@@ -1,4 +1,5 @@
 POLLS_TODAY = "polls/today/"
-RESTAURANTS = "polls/history/?from{FROM}&to={TO}"
+GET_RESTAURANTS = "polls/history/?from{FROM}&to={TO}"
 CREATE_USER = "auth/users/create"
 GET_TOKEN = "auth/token/login"
+RESTAURANTS = "restaurants/"
